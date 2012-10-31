7e9af6931fcb8598a3d721ad950164c98e88de80
Shari Evans <shari.evans@comcast.net>
2012-10-30 21:57:49 -0400

    My second dojo having cloud 9 problems

--- homework-9.1.php
+++ homework-9.1.php
@@ -1,12 +1,20 @@
-
 <?php
 
-function add ($a, $b)
+
+function add ($a , $b)
 {
     return ($a + $b);
 }
 
 
-assert(add (1,1) == 2);
+
+assert (add (1,1) == 2);
 
 
+function sub ($a , $b)
+{
+    return ($a - $b);
+}
+
+
+assert (sub (2, 1) == 1);
