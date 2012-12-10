f1895d3d2f729e12cdd773472bdfc5b317b6bba0
Shari Evans <shari.evans@comcast.net>
2012-12-09 20:57:49 -0500

    First Failing test

--- calculator-test.php
+++ calculator-test.php
@@ -1,9 +1,15 @@
 <?php
 
-include 'calculator.php';
+require_once "calculator.php";
 
 
+//I'm going o start with the function test provided in class
 
-if  (assert (add (1,1) == 2))
-    
-    echo "This is addition";
+function test ($assertion, $message = null)
+{
+    echo (@assert ($assertion)? 'pass!' : 'fail!') . $message . "/n";
+}
+
+
+// Failing test: Does the class Calculator exist?
+$calculator = new Calculator;
